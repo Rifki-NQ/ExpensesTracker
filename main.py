55813e2544d36a68f31c6004d5ad61afa6131f86
RIFKI-NQ <rifqinf950@gmail.com>
2025-12-04 04:28:21 +0700

    add quit option on main menu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("Personal expense tracker")
 print("1. Input monthly salary")
 
 while True:
-    index = input("Input by index: ")
+    index = input("Input by index (q for quit): ")
     if index.isdigit():
         index = int(index)
         if index == 1:
@@ -13,5 +13,7 @@ while True:
             break
         else:
             print("-- error: invalid index inputted!")
+    elif index.lower() == "q":
+        break
     else:
         print("-- error: use digits for the index!")
