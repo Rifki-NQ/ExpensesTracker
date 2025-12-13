a86077bdf57d3fade9ce73e4ae315f112cabd651
RIFKI-NQ <rifqinf950@gmail.com>
2025-12-13 13:40:08 +0700

    WIP: add feature show monthly expenses

--- core/merge_data.py
+++ core/merge_data.py
@@ -9,20 +9,19 @@ df_expenses = read(expensespath)
 df_salary["date"] = pd.to_datetime(df_salary["date"], format="%m-%Y")
 df_expenses["date"] = pd.to_datetime(df_expenses["date"], format="%d-%m-%Y")
 
-
-def split_date(df, ):
-    print()
-
 def show_monthly_expenses():
     if df_expenses.empty:
         print("Empty expenses data!")
         return
-    #process date
     monthly_expenses = pd.DataFrame(columns=["date", "Total expenses"])
+    #add values from expenses data
     monthly_expenses["date"] = df_expenses["date"].dt.strftime("%B %Y")
-    monthly_expenses["date"] = monthly_expenses["date"].mask(monthly_expenses["date"].duplicated())
-    monthly_expenses["date"] = pd.to_datetime(monthly_expenses["date"], format="%B %Y")
-    #process total expenses
     monthly_expenses["Total expenses"] = df_expenses["expense"]
+    #group by duplicated month then sum the expenses
+    monthly_expenses = monthly_expenses.groupby("date", as_index=False)["Total expenses"].sum()
+    #sort the date
+    monthly_expenses["date"] = pd.to_datetime(monthly_expenses["date"], format="%B %Y")
+    monthly_expenses = monthly_expenses.sort_values("date")
+    monthly_expenses ["date"]= monthly_expenses["date"].dt.strftime("%B %Y")
 
     print(monthly_expenses)
