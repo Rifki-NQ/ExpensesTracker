6e40992c597a8b3d054045864a1e9e99862dd6a0
RIFKI-NQ <rifqinf950@gmail.com>
2025-12-19 21:50:17 +0700

    Add helper function read expenses

--- core/merge_data.py
+++ core/merge_data.py
@@ -3,7 +3,7 @@ from .utils import read
 from .utils import salarypath, expensespath
 
 #helper function for read, sort and format date of salary
-def read_salary(date_format=None):
+def read_salary(date_format=""):
     df_salary = read(salarypath)
     if df_salary.empty:
         return df_salary
@@ -15,10 +15,33 @@ def read_salary(date_format=None):
     elif date_format == "%B %Y":
         df_salary["date"] = df_salary["date"].dt.strftime("%B %Y")
         return df_salary
+    #return actual, not formatted datetime type date
     else:
         return df_salary
 
-def monthly_summary(inlcude="all"):
+#helper function for read, sort, choose columns and format date of expenses
+def read_expenses(date_format="", include=""):
+    df_expenses = read(expensespath)
+    if df_expenses.empty:
+        return df_expenses
+    df_expenses["date"] = pd.to_datetime(df_expenses["date"], format="%d-%m-%Y")
+    df_expenses.sort_values("date", inplace=True)
+    if include == "date_salary":
+        df_expenses = df_expenses[["date", "salary"]]
+    elif include == "date_salary_category":
+        df_expenses = df_expenses[["date", "salary", "category"]]
+    #format the date
+    if date_format == "%d-%m-%Y":
+        df_expenses["date"] = df_expenses["date"].dt.strftime("%d-%m-%Y")
+        return df_expenses
+    elif date_format == "%B %Y":
+        df_expenses["date"] = df_expenses["date"].dt.strftime("%B %Y")
+        return df_expenses
+        #return actual, not formatted datetime type date
+    else:
+        return df_expenses
+
+def monthly_summary(include="all"):
     df_salary = read_salary(date_format="%B %Y")
     
     
